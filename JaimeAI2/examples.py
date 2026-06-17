9e7d64b5a98d1e53c4f82bb4f54a2d8b11bdc20b
jaime100200300 <jaime702ochoa@gmail.com>
2026-06-17 15:56:31 -0700

    uhmm

--- JaimeAI2/examples.py
+++
@@ -1,8 +0,0 @@
-# Project Maker
-from main import ask
-from printing import slow
-
-typeProject = input('Choose a project type: html, txt, or md > ')
-
-print()
-ask(f"make a project {typeProject}")
